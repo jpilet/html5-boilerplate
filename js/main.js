3d577411a23c467b58201d3dfb2be8dd80dbba94
Julien Pilet <julien.pilet@aptarism.com>
2013-10-11 08:09:48 +0200

    basic structure
    - removed some unused files;
    - page is working, with section swapping.
    - IE9+firefox+chrome+mobile tested.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 
+jQuery( function($){
+	function resizeSections() {
+		var height = window.innerHeight, width = window.innerWidth;
+		var headerHeight = $("#content")[0].offsetTop;
+		
+		$("#content").css({
+			"height" : height - headerHeight + "px",
+			"width" : width + "px"
+		});
+		
+		
+		$("section").css({
+			"min-height" : (height - headerHeight - 10) + "px",
+			"width" : (width - 10) + "px"
+		});
+		
+		
+	}
+	
+	function selectSection(section) {
+		$("section").css({
+			"display": "none"
+		});
+		$(section).css({"display":"block"});
+	}
+	
+	resizeSections();
+	window.onresize = _.debounce( resizeSections , 200);
+	
+	selectSection("#home");
+	
+	window.onhashchange = function() {
+		selectSection(location.hash);
+	};
+});
