22a589c091b6ddd2beb86b717b150871c2df328c
Julien Pilet <julien.pilet@aptarism.com>
2013-10-15 09:43:34 +0200

    circular placement, geometry improvements.
    sections placed in circle
    fixed perspective directive in css
    code refactory
    center sections on screen

--- js/main.js
+++ js/main.js
@@ -1,44 +1,50 @@
 
 jQuery( function($){
+	var geometry = {
+		sectionWidth: 0,
+		sectionHeight: 0,
+		clipZoneWidth: 0,
+		clipZoneHeight: 0,
+		bounds: {
+			width: 0,
+			height: 0,
+		}
+	};
+			
 	function computeBounds() {
-		var bounds = {
-			maxX: -1000,
-		    minX: 1000,
-		    maxY: -1000,
-		    minY: 1000
-		  };
+		var bounds = geometry.bounds;
+		var maxX = -1000;
+		var minX = 1000;
+		var maxY = -1000;
+		var minY = 1000;
+		
+		// sectionPosition depends on bounds.width and bounds.height, which are
+		// not known at this point. However, it is just a translation
+		// that has no impact on width and height.
+		bounds.width = 0;
+		bounds.height = 0;
 		$("section").each(function() {
-			var x = this.getAttribute("data-x");
-			var y = this.getAttribute("data-y");
-			bounds.maxX = Math.max(bounds.maxX, x);
-			bounds.minX = Math.min(bounds.minX, x);
-			bounds.maxY = Math.max(bounds.maxY, y);
-			bounds.minY = Math.min(bounds.minY, y);
+			var pos = sectionPosition(this);
+			var x = pos.left;
+			var y = pos.top;
+			maxX = Math.max(maxX, x + geometry.sectionWidth);
+			minX = Math.min(minX, x);
+			maxY = Math.max(maxY, y + geometry.sectionHeight);
+			minY = Math.min(minY, y);
 		});
-		bounds.width = bounds.maxX - bounds.minX + 1;
-		bounds.height = bounds.maxY - bounds.minY + 1;
-		return bounds;
+		bounds.width = Math.max(maxX * 2, - minX * 2);
+		bounds.height = Math.max(maxY * 2, - minY * 2);
 	}
-	var bounds = computeBounds();
-	var horizontalSeparation = 0;
-	var verticalSeparation = 0;
-	
-	function placeSections(_horizontalSeparation, _verticalSeparation) {
-		horizontalSeparation = _horizontalSeparation;
-		verticalSeparation = _verticalSeparation;
 		
-		var sections = $("section");
-		
+	function placeSections() {		
+		computeBounds();
 		$("#content").css({
-			"width": bounds.width * horizontalSeparation,
-			"height": bounds.height * verticalSeparation,
+			"width": geometry.bounds.width,
+			"height": geometry.bounds.height,
 		});
 		
 		$("section").each(function() {
-			$(this).css({
-				"left": this.getAttribute("data-x") * horizontalSeparation,
-			    "top": this.getAttribute("data-y") * verticalSeparation
-			});
+			$(this).css(sectionPosition(this));
 		});
 	}
 
@@ -47,21 +53,21 @@ jQuery( function($){
 		var clipZone = $("#clipZone");
 		var headerHeight = clipZone[0].offsetTop;
 		
-		var clipZoneWidth = width;
-		var clipZoneHeight = (height - headerHeight);
+		geometry.clipZoneWidth = width;
+		geometry.clipZoneHeight = (height - headerHeight);
 		clipZone.css({
-			"height": clipZoneHeight + "px",
-			"width" : clipZoneWidth + "px"
+			"height": geometry.clipZoneHeight + "px",
+			"width" : geometry.clipZoneWidth + "px"
 		});
 		
-		var sectionWidth = clipZoneWidth * .5;
-		var sectionHeight = clipZoneHeight * .5;
+		geometry.sectionWidth = geometry.clipZoneWidth * .8;
+		geometry.sectionHeight = geometry.clipZoneHeight * .8;
 		$("section").css({
-			"height" : sectionHeight + "px",
-			"width" : sectionWidth + "px",
+			"height" : geometry.sectionHeight + "px",
+			"width" : geometry.sectionWidth + "px"
 		});
 		
-		placeSections(clipZoneWidth * .8, clipZoneHeight * .8);
+		placeSections();
 		selectSection();
 	}
 	
@@ -70,14 +76,32 @@ jQuery( function($){
 	}
 	
 	function sectionPosition(section) {
+		var angle = $(section).attr("data-angle") * Math.PI / 180.0;
+		var sin = Math.sin(angle);
+		var cos = Math.cos(angle);
+		var r = $(section).attr("data-radius") * 1.5;
+
 		return {
-			left: -(section.attr("data-x") * horizontalSeparation) + 100,
-			top: -(section.attr("data-y") * verticalSeparation) + 100
+			left: cos * r * geometry.sectionWidth - (geometry.sectionWidth / 2) + geometry.bounds.width / 2,
+			top: sin * r * geometry.sectionHeight - (geometry.sectionHeight / 2) + geometry.bounds.height / 2
 		};
 	}
 	
+	function translationForSection(section) {
+		var pos = sectionPosition(section);
+		return {
+			left: (geometry.clipZoneWidth - geometry.sectionWidth) / 2 - pos.left,
+			top: (geometry.clipZoneHeight - geometry.sectionHeight) / 2 - pos.top
+		};
+	}
+
 	var selectedSection = null;
 	function selectSection() {
+		// Browsers try to scroll to the correct location.
+		// Make sure to reset scrolling, we'll pace stuff ourselves.
+		$("#clipZone")[0].scrollLeft = 0;
+		$("#clipZone")[0].scrollTop = 0;
+		
 		var section = currentSection();
 		var previousSection = selectedSection;
 		
@@ -86,11 +110,11 @@ jQuery( function($){
 		}
 		selectedSection = section;
 		
-		var pos = sectionPosition(section);
-		$("#clipZone")[0].scrollLeft = 0;
-		$("#clipZone")[0].scrollTop = 0;
+		var pos = translationForSection(section);
+
 		if (previousSection && Modernizr.cssanimations) {
-			var prevPos = sectionPosition(previousSection);
+			var prevPos = translationForSection(previousSection);
+
 			var mid = {
 				left: (prevPos.left + pos.left) / 2,
 				top: (prevPos.top + pos.top) / 2
