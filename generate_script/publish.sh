889ac995f86f7b5dacca697799f734ac52df558e
Julien Pilet <julien.pilet@gmail.com>
2013-11-18 09:37:25 +0100

    Adds a publication script.

---
+++ generate_script/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(cd "$(dirname "$0")/.." ; pwd)
+
+TMP="/tmp/opticode-ftp-upload"
+
+rm -fR "${TMP}"
+mkdir "${TMP}"
+
+rsync -ar \
+    --exclude "doc" \
+    --exclude "generate_script" \
+    --exclude "*.md" \
+    --exclude ".git*" \
+    --exclude ".DS_Store" \
+    --exclude ".project" \
+    "${ROOT}" "${TMP}"
+
+ncftpput -z -R opticode www "${TMP}/opticode.ch/"*
+
+rm -fR "${TMP}"
